6130e1884a6012b88e2c4e019f2239a70bdcac01
Gem <mojeedibrahim13@gmail.com>
2025-12-07 05:04:14 -0800

    learning dart easyily

--- dart_package_1_example.dart
+++ dart_package_1_example.dart
@@ -1,9 +1,24 @@
-import 'package:dart_package_1/dart_package_1.dart';
+
 
 // void main() {
 //   var awesome = Awesome();
 //   print('awesome: ${awesome.isAwesome}');
 // }
 void main() {
-  print("Hello World!!!");
+  var name = "Abdulmojeed Ibrahim";
+  print("My name is: $name");
+
+  List food1 = ["Yam", "Rice", "Beans", "Akara"];
+  for (int i=0; i<food1.length; i++){
+    print(food1[i]);
+  }  
+  List food2 = ["Semo", "Amala", "Garri", "Fufu"];
+  for (var food2 in food2){
+    print(food2);
+  }
+
+  var allFood1 = List.from(food1);
+  print(allFood1);
+  food2[2] = "Apple";
+  print(food2);
 }
